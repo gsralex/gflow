c35fe6a70eb71e8196bd00b1b07c9dda651fd2b8
gsralex <gsralex@163.com>
2020-03-18 23:24:24 +0800

    去掉无法编译的代码

--- src/main/java/com/gsralex/gflow/executor/client/ExecutorClient.java
+++ src/main/java/com/gsralex/gflow/executor/client/ExecutorClient.java
@@ -1,46 +1,36 @@
 package com.gsralex.gflow.executor.client;
 
-import com.gsralex.gflow.common.protocol.GFlowMessage;
-import com.gsralex.gflow.common.protocol.GenericDecoder;
-import com.gsralex.gflow.executor.handler.ExecutorClientHandler;
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-
 /**
  * @author gsralex
  * @date 2020/1/29
  */
 public class ExecutorClient {
 
-    public void initialize(){
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        Bootstrap bootstrap = new Bootstrap();
-        bootstrap
-                // 1.指定线程模型
-                .group(workerGroup)
-                // 2.指定 IO 类型为 NIO
-                .channel(NioSocketChannel.class)
-                // 3.IO 处理逻辑
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel ch) {
-                        ch.pipeline().addLast(new ExecutorClientHandler());
-                        ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 0))
-                                .addLast(new GenericDecoder(GFlowMessage.class));
-                    }
-                });
-        // 4.建立连接
-        bootstrap.connect("localhost", 80).addListener(future -> {
-            if (future.isSuccess()) {
-                System.out.println("连接成功!");
-            } else {
-                System.err.println("连接失败!");
-            }
-        });
-    }
+//    public void initialize(){
+//        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+//
+//        Bootstrap bootstrap = new Bootstrap();
+//        bootstrap
+//                // 1.指定线程模型
+//                .group(workerGroup)
+//                // 2.指定 IO 类型为 NIO
+//                .channel(NioSocketChannel.class)
+//                // 3.IO 处理逻辑
+//                .handler(new ChannelInitializer<SocketChannel>() {
+//                    @Override
+//                    public void initChannel(SocketChannel ch) {
+//                        ch.pipeline().addLast(new ExecutorClientHandler());
+//                        ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 0))
+//                                .addLast(new GenericDecoder(GFlowMessage.class));
+//                    }
+//                });
+//        // 4.建立连接
+//        bootstrap.connect("localhost", 80).addListener(future -> {
+//            if (future.isSuccess()) {
+//                System.out.println("连接成功!");
+//            } else {
+//                System.err.println("连接失败!");
+//            }
+//        });
+//    }
 }
--- src/main/java/com/gsralex/gflow/executor/server/ExecutorServer.java
+++ src/main/java/com/gsralex/gflow/executor/server/ExecutorServer.java
@@ -1,8 +1,6 @@
 package com.gsralex.gflow.executor.server;
 
 import com.gsralex.gflow.common.config.GFlowConfig;
-import com.gsralex.gflow.common.protocol.GFlowMessage;
-import com.gsralex.gflow.common.protocol.GenericDecoder;
 import com.gsralex.gflow.common.spring.SpringContextUtils;
 import com.gsralex.gflow.executor.handler.ExecutorServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
@@ -10,7 +8,6 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,8 +38,6 @@ public class ExecutorServer {
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) {
-                        ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 0))
-                                .addLast(new GenericDecoder(GFlowMessage.class));
                         ch.pipeline().addLast(executorServerHandler);
                     }
                 });
